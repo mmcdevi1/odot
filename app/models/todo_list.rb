fc1134a1b862930c33bb882561a626890347277a
Michael McDevitt <mmcdevi1@gmail.com>
2014-10-26 18:00:12 -0400

    clean up show view

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -5,4 +5,12 @@ class TodoList < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 5 }
 
+  def has_completed_items?
+    todo_items.complete.size > 0
+  end
+
+  def has_incompleted_items?
+    todo_items.incomplete.size > 0
+  end
+
 end
